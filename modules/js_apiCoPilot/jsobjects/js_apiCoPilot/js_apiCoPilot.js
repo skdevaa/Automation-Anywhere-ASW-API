3c7691c130f6820284f63df16aaae0c0599554a7
stefan.karsten <stefan.karsten@automationanywhere.com>
2025-11-21 08:00:08 +0000

    CP Update

--- modules/js_apiCoPilot/jsobjects/js_apiCoPilot/js_apiCoPilot.js
+++ modules/js_apiCoPilot/jsobjects/js_apiCoPilot/js_apiCoPilot.js
@@ -1,39 +1,47 @@
 export default {
-	
+
 	// !! DO NOT USE - Experimental
 
-		extractLabelValues(data) { 
+	extractLabelValues(data) { 
 
-			const formObj = typeof data.form === "string" ? JSON.parse(data.form) : data.form;
-			const result = {};
-			const rows = formObj?.form?.rows || [];
+		const formObj = typeof data.form === "string" ? JSON.parse(data.form) : data.form;
+		const result = {};
+		const rows = formObj?.form?.rows || [];
 
-			rows.forEach(row => {
-				(row.columns || []).forEach(col => {
-					if (col.label !== undefined && col.defaultValue !== undefined) {
-						result[col.label] = col.defaultValue;
-					}
-				});
+		rows.forEach(row => {
+			(row.columns || []).forEach(col => {
+				if (col.label !== undefined && col.defaultValue !== undefined) {
+					result[col.label] = col.defaultValue;
+				}
 			});
-			return result;
-		},
-	
+		});
+		return result;
+	},
+
 	async getInitForm(automationId,token) {
-    let response = await qry_getcopilotinitform_XT.run({ automationId: automationId , token :token});
+		let response = await qry_getCPinitform_XT.run({ automationId: automationId , token :token});
 		return response
 	},
-	
-		async getAutomation(token) {
-    let response = await qry_getCPProcesses_XT.run({ token :token});
-		
+
+	async getAutomation(token) {
+		let response = await qry_getCPProcesses_XT.run({ token :token});
+
 		const processes = Object.values(response.automationDetails).map(process => ({
 			code: process.id,
 			name: process.name
 		}));
 		return processes
 	},
-	
-	
 
-	
+
+
+	async getRequests(token,filterOperand,titleFilter,statusFilter) {
+		let response = await qry_getCPInstances_XT.run({ token: token, filterOperand: filterOperand, statusFilter: titleFilter, titleFilter: statusFilter })
+
+		return response.list
+
+	}
+
+
+
 }
